7940b66b22d2797428742c8282c878cf1c283991
pozafly <pozafly@gmail.com>
2025-01-11 12:17:56 +0900

    test: member service test 코드 작성

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,29 @@
+package hello.core.member;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MemberServiceTest {
+
+    private MemberService memberService;
+
+    @BeforeEach
+    void beforeEach() {
+        memberService = new MemberServiceImpl();
+    }
+
+    @Test
+    void join() {
+        // given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        // when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        // then
+        assertThat(findMember).isEqualTo(member);
+    }
+}
