503d9eaabf0006ae9405ef57a2f2c009ff0f297f
pozafly <pozafly@gmail.com>
2025-01-13 21:26:11 +0900

    feat: configuration 설정

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -22,4 +22,8 @@ public class OrderServiceImpl implements OrderService {
         Order order = new Order(memberId, itemName, itemPrice, discountPrice);
         return order;
     }
+
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
